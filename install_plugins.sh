72f8318e0609a3dcec42f04fa0f0ccf9172160a9
Fabio <bilotto@github.com>
2024-07-23 16:01:28 -0300

    chore: Update Jenkins image version and install additional tools

--- install_plugins.sh
+++ install_plugins.sh
@@ -1,25 +1,19 @@
+# curl -u $JENKINS_USERNAME:$JENKINS_TOKEN $JENKINS_URL/api/json
 
-TOKEN="11ce160cb6afaef141faa9391eeec5aeca"
-
-JENKINS_URL=http://172.18.159.22:30095
-USERNAME=user
-PASSWORD=121416
-
-curl -u $USERNAME:$TOKEN $JENKINS_URL/api/json
-
+rm jenkins-cli.jar*
 wget ${JENKINS_URL}/jnlpJars/jenkins-cli.jar
 
-java -jar jenkins-cli.jar -s $JENKINS_URL -auth $USERNAME:$PASSWORD groovy ./scripts/generateToken.groovy
+# java -jar jenkins-cli.jar -s $JENKINS_URL -auth $JENKINS_USERNAME:$JENKINS_PASSWORD groovy ./scripts/generateToken.groovy
+# java -jar jenkins-cli.jar -s $JENKINS_URL -auth $JENKINS_USERNAME:$JENKINS_PASSWORD install-plugin <nome-do-plugin>
+
+ALL_PLUGINS=$(cat plugins_out.txt)
+for plugin in $ALL_PLUGINS; do
+    echo "Installing $plugin"
+    java -jar jenkins-cli.jar -s $JENKINS_URL -auth $JENKINS_USERNAME:$JENKINS_PASSWORD install-plugin $plugin
+    # sleep 1
+done
+
+java -jar jenkins-cli.jar -s $JENKINS_URL -auth $JENKINS_USERNAME:$JENKINS_PASSWORD safe-restart
 
 
-
-java -jar jenkins-cli.jar -s $JENKINS_URL -auth $USERNAME:$PASSWORD install-plugin <nome-do-plugin>
-
-while read plugin; do
-    java -jar jenkins-cli.jar -s $JENKINS_URL -auth $USERNAME:$PASSWORD install-plugin $plugin
-done < plugins.txt
-
-java -jar jenkins-cli.jar -s $JENKINS_URL -auth $USERNAME:$PASSWORD safe-restart
-
-
-java -jar jenkins-cli.jar -s $JENKINS_URL -auth $USERNAME:$PASSWORD groovy = < get-plugins-info.groovy
+# java -jar jenkins-cli.jar -s $JENKINS_URL -auth $JENKINS_USERNAME:$JENKINS_PASSWORD groovy = < get-plugins-info.groovy
--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import os
 import jenkins
 
-# Replace these with your Jenkins instance details
-jenkins_url= 'http://172.18.159.22:30095'
-username= 'user'
-api_token= '11ce160cb6afaef141faa9391eeec5aeca'
+# Read vars from ./env.profile
+jenkins_url = os.getenv('JENKINS_URL')
+username = os.getenv('JENKINS_USERNAME')
+api_token = os.getenv('JENKINS_TOKEN') 
+
 
 # Connect to Jenkins
 try:
@@ -14,15 +15,28 @@ except jenkins.BadHTTPException as e:
 except Exception as e:
     print('An error occurred:', str(e))
 
-try:
-    # Obter e imprimir a versão do Jenkins
-    version = server.get_version()
-    print('Versão do Jenkins:', version)
-except Exception as e:
-    print('An error occurred:', str(e))
+# try:
+#     # Obter e imprimir a versão do Jenkins
+#     version = server.get_version()
+#     print('Versão do Jenkins:', version)
+# except Exception as e:
+#     print('An error occurred:', str(e))
 
 
 # Get all installed plugins
 plugins = server.get_plugins()
-print(plugins)
+plugins_dict = dict()
+for k, v in plugins.items():
+    plugin_name = v['shortName']
+    plugin_version = v['version']
+    plugins_dict[plugin_name] = plugin_version
 
+import json
+# dump to plugins.json
+with open('plugins.json', 'w') as f:
+    json.dump(plugins_dict, f, indent=4)
+
+# Need to dump in the format one per line plugin:version
+with open('plugins_out.txt', 'w') as f:
+    for k, v in plugins_dict.items():
+        f.write(f'{k}:{v}\n')
